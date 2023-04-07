06e8201d3945039c96cdc418242c79c9949e76b7
wahyurhy <wahyurahayu82@gmail.com>
2023-04-07 23:33:14 +0700

    up to Part -4

--- app/src/main/java/com/pelitabangsa/upbandroidtv/MainActivity.java
+++ app/src/main/java/com/pelitabangsa/upbandroidtv/MainActivity.java
@@ -3,6 +3,9 @@ package com.pelitabangsa.upbandroidtv;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -10,7 +13,15 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.leanback.widget.BrowseFrameLayout;
 
+import com.pelitabangsa.upbandroidtv.fragment.GenresFragment;
+import com.pelitabangsa.upbandroidtv.fragment.LanguageFragment;
+import com.pelitabangsa.upbandroidtv.fragment.MovieFragment;
+import com.pelitabangsa.upbandroidtv.fragment.SearchFragment;
+import com.pelitabangsa.upbandroidtv.fragment.SettingsFragment;
+import com.pelitabangsa.upbandroidtv.fragment.SportsFragment;
+import com.pelitabangsa.upbandroidtv.fragment.TvShowFragment;
 import com.pelitabangsa.upbandroidtv.utils.Common;
+import com.pelitabangsa.upbandroidtv.utils.Constants;
 
 public class MainActivity extends FragmentActivity implements View.OnKeyListener {
 
@@ -22,8 +33,14 @@ public class MainActivity extends FragmentActivity implements View.OnKeyListener
     private TextView btnSettings;
     private TextView btnLanguage;
     private TextView btnGenre;
-    private boolean SIDE_MENU = false;
     private BrowseFrameLayout navBar;
+    private boolean SIDE_MENU = false;
+
+    Constants constants = new Constants();
+    private String selectedMenu = constants.MENU_HOME;
+
+    private View lastSelectedMenu = null;
+    private FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +55,8 @@ public class MainActivity extends FragmentActivity implements View.OnKeyListener
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, fragment);
         transaction.commit();
+
+        closeMenu();
     }
 
     private void initView() {
@@ -50,8 +69,13 @@ public class MainActivity extends FragmentActivity implements View.OnKeyListener
         btnLanguage = (TextView) findViewById(R.id.btn_language);
         btnGenre = (TextView) findViewById(R.id.btn_genre);
         navBar = (BrowseFrameLayout) findViewById(R.id.bflNavBar);
+        fragmentContainer = (FrameLayout) findViewById(R.id.container);
 
         initSetOnKeyListener();
+
+        lastSelectedMenu = btnHome;
+        lastSelectedMenu.setActivated(true);
+        changeFragment(new HomeFragment());
     }
 
     private void initSetOnKeyListener() {
@@ -68,11 +92,54 @@ public class MainActivity extends FragmentActivity implements View.OnKeyListener
     @Override
     public boolean onKey(View view, int i, KeyEvent keyEvent) {
         switch (i) {
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+
+                lastSelectedMenu.setActivated(false);
+                view.setActivated(true);
+                lastSelectedMenu = view;
+
+                switch (view.getId()) {
+                    case R.id.btn_search:
+                        selectedMenu = constants.MENU_SEARCH;
+                        changeFragment(new SearchFragment());
+                        break;
+                    case R.id.btn_home:
+                        selectedMenu = constants.MENU_HOME;
+                        changeFragment(new HomeFragment());
+                        break;
+                    case R.id.btn_tv:
+                        selectedMenu = constants.MENU_TV;
+                        changeFragment(new TvShowFragment());
+                        break;
+                    case R.id.btn_movies:
+                        selectedMenu = constants.MENU_MOVIE;
+                        changeFragment(new MovieFragment());
+                        break;
+                    case R.id.btn_sports:
+                        selectedMenu = constants.MENU_SPORTS;
+                        changeFragment(new SportsFragment());
+                        break;
+                    case R.id.btn_settings:
+                        selectedMenu = constants.MENU_SETTINGS;
+                        changeFragment(new SettingsFragment());
+                        break;
+                    case R.id.btn_language:
+                        selectedMenu = constants.MENU_LANGUAGE;
+                        changeFragment(new LanguageFragment());
+                        break;
+                    case R.id.btn_genre:
+                        selectedMenu = constants.MENU_GENRES;
+                        changeFragment(new GenresFragment());
+                        break;
+                }
             case KeyEvent.KEYCODE_DPAD_LEFT:
                 if (!SIDE_MENU) {
+                    switchToLastSelectedMenu();
+
                     openMenu();
                     SIDE_MENU = true;
                 }
+                break;
         }
         return false;
     }
@@ -88,7 +155,49 @@ public class MainActivity extends FragmentActivity implements View.OnKeyListener
         return super.onKeyDown(keyCode, event);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (SIDE_MENU) {
+            SIDE_MENU = false;
+            closeMenu();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void switchToLastSelectedMenu() {
+        switch (selectedMenu) {
+            case "search":
+                btnSearch.requestFocus();
+                break;
+            case "home":
+                btnHome.requestFocus();
+                break;
+            case "tv":
+                btnTv.requestFocus();
+                break;
+            case "movie":
+                btnMovies.requestFocus();
+                break;
+            case "sports":
+                btnSports.requestFocus();
+                break;
+            case "settings":
+                btnSettings.requestFocus();
+                break;
+            case "language":
+                btnLanguage.requestFocus();
+                break;
+            case "genres":
+                btnGenre.requestFocus();
+                break;
+        }
+    }
+
     private void openMenu() {
+        Animation animSlide = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
+        navBar.startAnimation(animSlide);
+
         navBar.requestLayout();
         navBar.getLayoutParams().width = Common.getWidthInPercent(this, 16);
     }
@@ -96,5 +205,8 @@ public class MainActivity extends FragmentActivity implements View.OnKeyListener
     private void closeMenu() {
         navBar.requestLayout();
         navBar.getLayoutParams().width = Common.getWidthInPercent(this, 5);
+
+        fragmentContainer.requestFocus();
+        SIDE_MENU = false;
     }
 }
---
+++ app/src/main/java/com/pelitabangsa/upbandroidtv/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.pelitabangsa.upbandroidtv.utils;
+
+public class Constants {
+    public final String MENU_SEARCH = "search";
+    public final String MENU_HOME = "home";
+    public final String MENU_TV = "tv";
+    public final String MENU_MOVIE = "movie";
+    public final String MENU_SPORTS = "sports";
+    public final String MENU_LANGUAGE = "language";
+    public final String MENU_GENRES = "genres";
+    public final String MENU_SETTINGS = "settings";
+
+    // intent tags
+
+    public final String INTENT_MENU = "manu";
+    public final String SELECTED_ID = "id";
+}
